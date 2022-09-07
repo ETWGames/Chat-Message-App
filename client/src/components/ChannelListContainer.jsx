d3eda21bc5523dc34841348ee2acdf7b1e8626b8
Nicholas Ali <nnicholass32@gmail.com>
2022-09-07 13:36:35 -0400

    Created SideBarContainer.jsx

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import { ChannelList, useChatContext } from 'stream-chat-react'
-import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
-import Cookies from 'universal-cookie'
-import MainIcon from '../assets/MainIcon.png'
+import React, { useState } from 'react';
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
-const SideBar = () => {
+//import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import MainIcon from '../assets/MainIcon.png'
+import LogoutIcon from '../assets/logout.png'
+
+const cookies = new Cookies();
+
+const SideBar = ({ logout }) => (
     <div className="channel-list__sidebar">
         <div className="channel-list__sidebar__icon1">
-            <div className="channel-list__sidebar__icon1__inner">
-                <img src={MainIcon} alt="Main Icon" width="30" />
+            <div className="icon1__inner">
+                <img src={MainIcon} alt="Hospital" width="30" />
+            </div>
+        </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner" onClick={logout}>
+                <img src={LogoutIcon} alt="Logout" width="30" />
             </div>
         </div>
     </div>
-}
+);
+
 
 const ChannelListContainer = () => {
   return (
-    <div>
-        ChannelListContainer
-    </div>
+    <>
+        <SideBar />
+    </>
   )
 }
 
