d176ae0892ff76702f8f92df5074dcb17a838398
Nicholas Ali <nnicholass32@gmail.com>
2022-09-07 13:22:14 -0400

    Initial App Setup

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react'
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './'
+import Cookies from 'universal-cookie'
+import MainIcon from '../assets/MainIcon.png'
+
+const SideBar = () => {
+    <div className="channel-list__sidebar">
+        <div className="channel-list__sidebar__icon1">
+            <div className="channel-list__sidebar__icon1__inner">
+                <img src={MainIcon} alt="Main Icon" width="30" />
+            </div>
+        </div>
+    </div>
+}
 
 const ChannelListContainer = () => {
   return (
